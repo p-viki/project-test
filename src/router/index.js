7649ff100e56f5da3152fbfbbde9907d4a046aa3
HuWeikai <2499163098@qq.com>
2023-04-25 15:29:50 +0800

    fuck:"清空项目"

--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-import * as VueRouter from 'vue-router'
-import routes from './routes'
-
-const router = VueRouter.createRouter({
-    history: VueRouter.createWebHistory(),
-    routes,
-})
-
-export default router
